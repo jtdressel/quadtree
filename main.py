a16b8b49c1446d9cd8965d31abc19a3d9068aa2b
James Dressel <ubergeek@jamesdressel.com>
2013-04-23 16:12:39 -0400

    Create rects in main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import timeit
-
 setup = '''
+import pygame
 import random
 
-random.seed('slartibartfast')
-s = [random.random() for i in range(100)]
-timsort = list.sort
 
+d = pygame.Rect(0,0,10,10)
+b = pygame.Rect(12, 12, 10, 10)
+c = pygame.Rect(5, 5, 10, 10)
+print c
+print d
+print b
+
+s = [random.random() for i in range(100)]
+
+timsort = list.sort
 '''
+
+
+
+
+
 
 print min(timeit.Timer('a=s[:]; timsort(a)', setup=setup).repeat(7, 100))
